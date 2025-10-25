b55aa2fa7c28c0b6e01f728aac6b06ca83b9c54a
Jadyn Ndirangu <jamesjadyn140@gmail.com>
2025-10-25 09:29:07 +0300

    Added the .cvs files and made the database schema

---
+++ data/README.txt
@@ -0,0 +1,5 @@
+Employee Salaries - Analysis tasks:
+1. Compute average salary per department.
+2. Find salary growth/trend for selected employees (time series).
+3. Identify top 5 highest paid employees and departments with highest total payroll.
+4. Visualizations: line chart (monthly payroll), bar chart (avg dept salary), box plot (salary distribution).
--- sql/database_schema.sql
+++ sql/database_schema.sql
@@ -2,4 +2,20 @@
 CREATE DATABASE IF NOT EXISTS company_payroll;
 USE company_payroll;
 
---Table to store employee information(to be added later)
+--Table to store employee information
+CREATE Table Employees(
+    employee_id VARCHAR(10) PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    department VARCHAR(50)NOT NULL,
+    hire_date DATE NOT NULL,
+    
+);
+--Table to store salary information
+CREATE TABLE Salaries(
+    salary_id VARCHAR(10) PRIMARY KEY,
+    employee_id VARCHAR(10) NOT NULL,
+    month VARCHAR(7) NOT NULL,
+    amount DECIMAL(10, 2) NOT NULL,
+    FOREIGN KEY (employee_id) REFERENCES Employees(employee_id)
+);
