76e9ab7236dbc79c5676fb7fb8f1548bec41d966
Jadyn Ndirangu <jamesjadyn140@gmail.com>
2025-10-26 13:36:54 +0300

    Add initial implementation of DataImportTool class

---
+++ src/main/java/com/group3/payroll/DataImportTool.java
@@ -0,0 +1,12 @@
+package main.java.com.group3.payroll;
+//import necessary libraries
+import javax.swing.*;
+import javax.swing.filechooser.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+
+public class DataImportTool {
+    //will add GUI components after learning more about Swing
+}
